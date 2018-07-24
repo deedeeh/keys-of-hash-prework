c7da0cc1b6d3869b638de9fe0cb0c018c3a83726
Dina Hafez <dinahafez.8@gmail.com>
2018-07-24 12:49:39 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,10 @@
-class Hash
-  def keys_of(*arguments)
-    # code goes here
-    result = []
-    self.each do |k, v|
-      arguments.map {|value| result << k if value == v}
-    end
-    result
-  end
-end
+# class Hash
+#   def keys_of(*arguments)
+#     # code goes here
+#     result = []
+#     self.each do |k, v|
+#       arguments.map {|value| result << k if value == v}
+#     end
+#     result
+#   end
+# end
